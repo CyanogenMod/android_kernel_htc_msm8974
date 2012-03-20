2a4d2ea4bf2c18ba0a5e02fd03697be74077811c
Eric Paris <eparis@redhat.com>
2012-03-20 14:35:12 -0400

    SELinux: allow default source/target selectors for user/role/range
    When new objects are created we have great and flexible rules to
    determine the type of the new object.  We aren't quite as flexible or
    mature when it comes to determining the user, role, and range.  This
    patch adds a new ability to specify the place a new objects user, role,
    and range should come from.  For users and roles it can come from either
    the source or the target of the operation.  aka for files the user can
    either come from the source (the running process and todays default) or
    it can come from the target (aka the parent directory of the new file)
    
    examples always are done with
    directory context: system_u:object_r:mnt_t:s0-s0:c0.c512
    process context: unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023
    
    [no rule]
    	unconfined_u:object_r:mnt_t:s0   test_none
    [default user source]
    	unconfined_u:object_r:mnt_t:s0   test_user_source
    [default user target]
    	system_u:object_r:mnt_t:s0       test_user_target
    [default role source]
    	unconfined_u:unconfined_r:mnt_t:s0 test_role_source
    [default role target]
    	unconfined_u:object_r:mnt_t:s0   test_role_target
    [default range source low]
    	unconfined_u:object_r:mnt_t:s0 test_range_source_low
    [default range source high]
    	unconfined_u:object_r:mnt_t:s0:c0.c1023 test_range_source_high
    [default range source low-high]
    	unconfined_u:object_r:mnt_t:s0-s0:c0.c1023 test_range_source_low-high
    [default range target low]
    	unconfined_u:object_r:mnt_t:s0 test_range_target_low
    [default range target high]
    	unconfined_u:object_r:mnt_t:s0:c0.c512 test_range_target_high
    [default range target low-high]
    	unconfined_u:object_r:mnt_t:s0-s0:c0.c512 test_range_target_low-high
    
    Signed-off-by: Eric Paris <eparis@redhat.com>
    
    Conflicts:
    	security/selinux/ss/services.c
    
    Change-Id: I9b8eac6790bbf48d35a049aaed717403aec02839

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -1256,6 +1256,7 @@ static int security_compute_sid(u32 ssid,
 				u32 *out_sid,
 				bool kern)
 {
+	struct class_datum *cladatum = NULL;
 	struct context *scontext = NULL, *tcontext = NULL, newcontext;
 	struct role_trans *roletr = NULL;
 	struct avtab_key avkey;
@@ -1304,12 +1305,20 @@ static int security_compute_sid(u32 ssid,
 		goto out_unlock;
 	}
 
+	if (tclass && tclass <= policydb.p_classes.nprim)
+		cladatum = policydb.class_val_to_struct[tclass - 1];
+
 	
 	switch (specified) {
 	case AVTAB_TRANSITION:
 	case AVTAB_CHANGE:
-		
-		newcontext.user = scontext->user;
+		if (cladatum && cladatum->default_user == DEFAULT_TARGET) {
+			newcontext.user = tcontext->user;
+		} else {
+			/* notice this gets both DEFAULT_SOURCE and unset */
+			/* Use the process user identity. */
+			newcontext.user = scontext->user;
+		}
 		break;
 	case AVTAB_MEMBER:
 		
@@ -1317,15 +1326,24 @@ static int security_compute_sid(u32 ssid,
 		break;
 	}
 
-	
-	if ((tclass == policydb.process_class) || (sock == true)) {
-		
+	/* Set the role to default values. */
+	if (cladatum && cladatum->default_role == DEFAULT_SOURCE) {
 		newcontext.role = scontext->role;
+	} else if (cladatum && cladatum->default_role == DEFAULT_TARGET) {
+		newcontext.role = tcontext->role;
+	} else {
+		if ((tclass == policydb.process_class) || (sock == true))
+			newcontext.role = scontext->role;
+		else
+			newcontext.role = OBJECT_R_VAL;
+	}
+
+	/* Set the type to default values. */
+	if ((tclass == policydb.process_class) || (sock == true)) {
+		/* Use the type of process. */
 		newcontext.type = scontext->type;
 	} else {
-		
-		newcontext.role = OBJECT_R_VAL;
-		
+		/* Use the type of the related object. */
 		newcontext.type = tcontext->type;
 	}
 
