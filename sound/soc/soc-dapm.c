225a94d9b416b54af1c7db9b61f27c0b01558281
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2014-02-11 11:48:53 -0800

    ASoC: dapm: Unlock mutex in error cases
    Dapm functions are not unlocking the mutex when an error
    is encountered.This change fixes the mutex unlock issue.
    
    Change-Id: Ia7ffaea9a30ae09a501f8f861c62c64d6c110d44
    CRs-fixed: 613644
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>
    (cherry picked from commit e5d642a37a643bf9ee4606e0a43b6714019fbc4a)

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2139,6 +2139,7 @@ int snd_soc_dapm_add_routes(struct snd_soc_dapm_context *dapm,
 		if (ret < 0) {
 			dev_err(dapm->dev, "Failed to add route %s->%s\n",
 				route->source, route->sink);
+			mutex_unlock(&dapm->card->dapm_mutex);
 			return ret;
 		}
 		route++;
@@ -2709,6 +2710,7 @@ int snd_soc_dapm_new_controls(struct snd_soc_dapm_context *dapm,
 			dev_err(dapm->dev,
 				"ASoC: Failed to create DAPM control %s: %d\n",
 				widget->name, ret);
+			mutex_unlock(&dapm->card->dapm_mutex);
 			return ret;
 		}
 		widget++;
