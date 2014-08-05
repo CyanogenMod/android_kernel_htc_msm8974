84709422caf61de7e7cfb538224556d519e6781a
Dan Pasanen <dan.pasanen@gmail.com>
2014-08-05 15:16:43 -0500

    m8: disable CONFIG_HTC_PNPMGR
    Change-Id: I4612d98e2e2c0f0cce91c76e4659ad144cf3c65a

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -35,7 +35,9 @@
 #define UPDATE_BUSY_VAL		1000000
 #define UPDATE_BUSY		50
 
+#ifdef CONFIG_HTC_PNPMGR
 extern void set_gpu_clk(unsigned int);
+#endif
 #define INIT_UDELAY		200
 #define MAX_UDELAY		2000
 
@@ -1062,9 +1064,10 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 		pwr->pwrlevels[i].io_fraction =
 			pdata->pwrlevel[i].io_fraction;
 	}
+#ifdef CONFIG_HTC_PNPMGR
 	if (strstr(device->name, "kgsl-3d") != NULL)
 		set_gpu_clk(pwr->pwrlevels[0].gpu_freq);
-
+#endif
 	
 	if (pwr->pwrlevels[0].gpu_freq > 0)
 		clk_set_rate(pwr->grp_clks[0], pwr->
