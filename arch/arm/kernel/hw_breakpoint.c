a6036ac1c4d38ed415fd1815e10e136277d42a50
Jurica Vukadin <u.ra604@gmail.com>
2014-12-16 21:13:21 +0100

    ARM: hw-breakpoint: Shut up, nobody cares
    Change-Id: I0552431ea571e4e66e381677a9d248699bfe6bfb

--- arch/arm/kernel/hw_breakpoint.c
+++ arch/arm/kernel/hw_breakpoint.c
@@ -874,7 +874,7 @@ static int debug_reg_trap(struct pt_regs *regs, unsigned int instr)
 {
 	int cpu = smp_processor_id();
 
-	pr_warning("Debug register access (0x%x) caused undefined instruction on CPU %d\n",
+	pr_debug("Debug register access (0x%x) caused undefined instruction on CPU %d\n",
 		   instr, cpu);
 
 	/* Set the error flag for this CPU and skip the faulting instruction. */
