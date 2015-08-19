9ab6641517730648e36789fccafdf4de8d0a40e3
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-19 15:12:14 -0500

    Revert "kernel/power/powersuspend: default to userspace for now"
    This reverts commit d6b6aca8ef880c4bbcaaaed26690e120f53e3464.

--- kernel/power/powersuspend.c
+++ kernel/power/powersuspend.c
@@ -247,7 +247,8 @@ static struct attribute_group power_suspend_attr_group =
 
 static struct kobject *power_suspend_kobj;
 
-// ------------------ sysfs interface -----------------------
+// ------------------------------------------ sysfs interface ------------------------------------------
+
 static int __init power_suspend_init(void)
 {
 
@@ -275,7 +276,7 @@ static int __init power_suspend_init(void)
 		return -ENOMEM;
 	}
 
-	mode = POWER_SUSPEND_USERSPACE;
+	mode = POWER_SUSPEND_KERNEL; // Yank555.lu : Default to kernel mode
 
 	return 0;
 }
